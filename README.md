de3f53a2c319dfb9e60328681a6dae096bdebae9
dabzo <adam@dabzo.com>
2013-11-09 01:11:54 -0800

    Revising ReadMe

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ tabular
 =======
 
 Tabbed Widget Sidebar WordPress plugin
+
+Widgets Must have titles for this plugin to work. 
+Failure to provide a title will result in the title-field not showing, breaking layout.
+
+Use [tabular] shortcode to include widgets added to the Tabbed-Sidebar in your WordPress theme. 
+Tabs will cycle automatically & are clickable.
--- totally-tabular.php
+++ totally-tabular.php
@@ -27,7 +27,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 ReadMe: Widgets Must have titles for this plugin to work. 
 Failure to provide a title will result in the title-field not showing, breaking layout.
-See Lines 33-36
+
+Use [tabular] shortcode to include widgets added to the Tabbed-Sidebar in your WordPress theme. 
+Tabs will cycle automatically & are clickable.
 */
 function tabular_sidebar(){
   register_sidebar( array(
