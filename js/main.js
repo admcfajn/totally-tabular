79de387fd4000b1ce342b9bf81e065d0e6c10e6c
dabzo <adam@dabzo.com>
2014-01-06 16:53:07 -0800

    Revert "fixing problem"
    This reverts commit c9f904df44e1afb34a6ac994f1b647b9620be751.

--- js/main.js
+++ js/main.js
@@ -23,15 +23,6 @@ var InfiniteRotator =
     $('#tab-container').height($('.tab-widget .tab-content').eq(currentItem).height() + verticalPadding);        
     $('.tab-widget .tab-content').eq(currentItem).fadeIn(fadeTime);
 
-  $('.tab-title').click(function(){
-	$('.tab-widget .tab-title').removeClass('current-item');
-	$(this).addClass('current-item');
-	$('.tab-widget .tab-content').hide();
-	$('#tab-container').height($(this).next('.tab-content').height() + verticalPadding).fadeIn(fadeTime);
-	$(this).next('.tab-content').show();
-	clearInterval(infiniteLoop);
-  }); 
-	
     //loop through the items
     var infiniteLoop = setInterval(function(){
       $('.tab-widget .tab-content').eq(currentItem).hide();
@@ -47,6 +38,14 @@ var InfiniteRotator =
       $('#tab-container').height($('.tab-widget .tab-content').eq(currentItem).height() + verticalPadding).fadeIn(fadeTime);        
       $('.tab-widget .tab-content').eq(currentItem).fadeIn(fadeTime);
 
+      $('.tab-title').click(function(){
+        $('.tab-widget .tab-title').removeClass('current-item');
+        $(this).addClass('current-item');
+        $('.tab-widget .tab-content').hide();
+        $('#tab-container').height($(this).next('.tab-content').height() + verticalPadding).fadeIn(fadeTime);
+        $(this).next('.tab-content').show();
+        clearInterval(infiniteLoop);
+      }); 
     }, itemInterval);
   }
 };
